b0e024bf38526385214c48bb6f9596337659818c
Sanket Kumar <sanketkumar@microsoft.com>
2022-06-20 18:06:14 +0530

    Initial Commit

---
+++ Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by Sanket Kumar on 20/06/22.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
