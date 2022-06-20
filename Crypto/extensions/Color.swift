dcdd309079f49a7e676ae8f57b45e5b6ff2028b9
Sanket Kumar <sanketkumar@microsoft.com>
2022-06-20 18:25:42 +0530

    Added Assets and Color Theme logic 🚀

---
+++ Crypto/extensions/Color.swift
@@ -0,0 +1,24 @@
+//
+//  Color.swift
+//  Crypto
+//
+//  Created by Sanket Kumar on 20/06/22.
+//
+
+import SwiftUI
+
+extension Color {
+	
+	static let theme = ColorTheme()
+}
+
+
+struct ColorTheme {
+	
+	let accent = Color("AccentColor")
+	let background = Color("BackgroundColor")
+	let green = Color("GreenColor")
+	let red = Color("RedColor")
+	let secondaryColor = Color("SecondaryTextColor")
+	
+}
